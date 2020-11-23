d58ee09ec996433cb8b7fb910ef200f4206acd9c
QuocDat1994 <quocdat1994@gmail.com>
2020-11-23 14:50:38 +0700

    finish Header and Home

--- src/components/AppHeader/AppHeader.js
+++ src/components/AppHeader/AppHeader.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Menu } from 'antd';
 import { Header } from 'antd/lib/layout/layout';
-import { Affix } from 'antd';
+import { Affix, Space } from 'antd';
 import "./AppHeader.less"
+import { Menu } from 'antd';
+
+// import { AppMenu } from './AppMenu/AppMenu';
 
 export const AppHeader = () => {
   const { Item } = Menu;
@@ -10,24 +12,31 @@ export const AppHeader = () => {
   return (
     <Affix offsetTop={0}>
       <Header className="AppHeader">
-        <a className="brand" href="#">
-          <img
-            src="https://preview.uideck.com/items/slick/business/img/logo.png"
-            alt="logo"
-          />
-        </a>
-        <Menu mode="horizontal" defaultSelectedKeys={"home"}>
-          <Item key="home">Home</Item>
-          <Item key="about">About</Item>
-          <Item key="services">Services</Item>
-          <Item key="showcase">Showcase</Item>
-          <Item key="pricing">Pricing</Item>
-          <Item key="team">Team</Item>
-          <Item key="blog">Blog</Item>
-          <Item key="contact">Contact</Item>
-        </Menu>
+        <div className="container">
+          <a className="logo" href="#localhost">
+            <img
+              src="https://preview.uideck.com/items/slick/business/img/logo.png"
+              alt="logo"
+            />
+          </a>
+          <Menu
+            className="AppMenu"
+            mode={"horizontal"}
+            defaultSelectedKeys={"home"}
+            forceSubMenuRender={true}
+            overflowedIndicator={<i className="fas fa-bars"></i>}
+          >
+            <Item key="home">Home</Item>
+            <Item key="about">About</Item>
+            <Item key="services">Services</Item>
+            <Item key="showcase">Showcase</Item>
+            <Item key="pricing">Pricing</Item>
+            <Item key="team">Team</Item>
+            <Item key="blog">Blog</Item>
+            <Item key="contact">Contact</Item>
+          </Menu>
+        </div>
       </Header>
     </Affix >
-
   )
 }
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,37 +1,39 @@
 import React from 'react'
-import { Row, Col, Button } from 'antd';
+import { Row, Col, Button, Space } from 'antd';
 import "./Home.less";
 
 export const Home = () => {
   return (
-    <div className="Home">
-      <Row className="container">
-        <Col xs={24} md={12}>
-          <h1 className="heading">Handcrafted Web Template For Business and Startups</h1>
-          <p className="text">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
-            dolores ea fugiat nesciunt quisquam.
-          </p>
-          <div>
-            <Button type="primary" className="button">
-              <a href="#">
-                Download
-              </a>
-            </Button>
-            <Button type="default" className="button">
-              <a href="#">
-                Contact US
-              </a>
-            </Button>
-          </div>
-        </Col>
-        <Col xs={24} md={12}>
-          <img
-            className="img-intro"
-            src="https://preview.uideck.com/items/slick/business/img/intro.png"
-            alt="intro" />
-        </Col>
-      </Row>
-    </div>
+    <section id="home" className="Home">
+      <div className="container">
+        <Row>
+          <Col xs={24} md={12}>
+            <h1 className="heading">Handcrafted Web Template For Business and Startups</h1>
+            <p className="text">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab
+              dolores ea fugiat nesciunt quisquam.
+            </p>
+            <Space >
+              <Button type="primary" className="button">
+                <a href="#local">
+                  Download
+                </a>
+              </Button>
+              <Button type="default" className="button">
+                <a href="#local">
+                  Contact US
+                </a>
+              </Button>
+            </Space >
+          </Col>
+          <Col xs={24} md={12}>
+            <img
+              className="img-intro"
+              src="https://preview.uideck.com/items/slick/business/img/intro.png"
+              alt="intro" />
+          </Col>
+        </Row>
+      </div>
+    </section >
   )
 }
